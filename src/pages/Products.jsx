52c0ed99530bc7eaa9672df4237b2d433f654d3f
dongree <dongwoo0307@naver.com>
2023-04-24 22:35:49 +0900

    create products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '../components/Card';
+import { useFirebase } from '../context/firebaseContext';
 
 export default function Products() {
-  return <div>Products</div>;
+  const [data, setData] = useState([]);
+  const { firebase } = useFirebase();
+
+  useEffect(() => {
+    firebase.getItems().then(d => setData(d));
+  }, []);
+
+  return (
+    <div className="grid grid-cols-4 gap-3 p-4">
+      {data.map(item => (
+        <Card info={item} key={item.id} />
+      ))}
+    </div>
+  );
 }
